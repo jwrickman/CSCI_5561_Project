ca281fa69ce44d0924b6431d6d65d133b6f9ddc3
Jack Rickman <rickm014@umn.edu>
2021-10-26 18:19:58 -0500

    train_example is the working trainer

---
+++ evaluation.py
@@ -0,0 +1,52 @@
+import numpy as np
+import h5py
+import json
+
+
+
+
+ANNOTATION_FILE = "/media/storage2/open_monkey/monkey_val_annotations.json"
+H5_FILE = "/media/storage2/open_monkey/val_data.h5"
+MODEL_FILE = "/media/storage2/open_monkey/checkpoints/Best_LEAP.h5"
+LOG_FILE = "/media/storage2/open_monkey/log.h5"
+
+def MPJPE(y_error):
+    result = np.sqrt(np.sum(y_error ** 2, axis=2))
+    result = np.mean(result, axis=0)
+    result = result / 128
+    return np.mean(result)
+
+
+def PCK(y_error, err):
+    result = np.sqrt(np.sum(y_error ** 2, axis=2))
+    result = (result / 128) <  err
+    result = np.mean(result, axis=1)
+    return np.mean(result)
+
+
+if __name__ == "__main__":
+
+    with h5py.File(LOG_FILE, "r") as results:
+        results = np.array(results["logs"]["y_error"])[1]
+        print(MPJPE(results))
+        print(PCK(results, 0.2))
+        print(PCK(results, 0.5))
+
+
+#    ### Load Data ###
+#    data_generator = DataGenerator(H5_FILE)
+#    train_generator = TrainingGenerator(generator=data_generator,
+#                                downsample_factor=0,
+#                                augmenter=augmenter,
+#                                sigma=5,
+#                                validation_split=0.1,
+#                                use_graph=True,
+#                                random_seed=2,
+#                                graph_scale=1)
+#
+#
+#    ### Load Model ###
+#    model = LEAP(train_generator)
+#
+#    for X, y in data_generator:
+#        out = model.predict(X, batch_size=1)
--- train_example.py
+++ train_example.py
@@ -20,7 +20,7 @@ from deepposekit.callbacks import Logger, ModelCheckpoint
 import time
 from os.path import expanduser
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 data_generator = DataGenerator("/media/storage2/open_monkey/train_data.h5")
 
@@ -69,25 +69,25 @@ sometimes = []
 augmenter = iaa.Sequential(augmenter)
 
 
-image, keypoints = data_generator[0]
-image, keypoints = augmenter(images=image, keypoints=keypoints)
-plt.figure(figsize=(5,5))
-image = image[0] if image.shape[-1] is 3 else image[0, ..., 0]
-cmap = None if image.shape[-1] is 3 else 'gray'
-plt.imshow(image, cmap=cmap, interpolation='none')
-for idx, jdx in enumerate(data_generator.graph):
-        if jdx > -1:
-                    plt.plot(
-                        [keypoints[0, idx, 0], keypoints[0, jdx, 0]],
-                        [keypoints[0, idx, 1], keypoints[0, jdx, 1]],
-                        'r-'
-                    )
-plt.scatter(keypoints[0, :, 0], keypoints[0, :, 1], c=np.arange(data_generator.keypoints_shape[0]), s=50, cmap=plt.cm.hsv, zorder=3)
-plt.show()
+#image, keypoints = data_generator[0]
+#image, keypoints = augmenter(images=image, keypoints=keypoints)
+#plt.figure(figsize=(5,5))
+#image = image[0] if image.shape[-1] is 3 else image[0, ..., 0]
+#cmap = None if image.shape[-1] is 3 else 'gray'
+#plt.imshow(image, cmap=cmap, interpolation='none')
+#for idx, jdx in enumerate(data_generator.graph):
+#        if jdx > -1:
+#                    plt.plot(
+#                        [keypoints[0, idx, 0], keypoints[0, jdx, 0]],
+#                        [keypoints[0, idx, 1], keypoints[0, jdx, 1]],
+#                        'r-'
+#                    )
+#plt.scatter(keypoints[0, :, 0], keypoints[0, :, 1], c=np.arange(data_generator.keypoints_shape[0]), s=50, cmap=plt.cm.hsv, zorder=3)
+#plt.show()
 
 
 train_generator = TrainingGenerator(generator=data_generator,
-                                downsample_factor=0,
+                                downsample_factor=2,
                                 augmenter=augmenter,
                                 sigma=5,
                                 validation_split=0.1,
@@ -97,33 +97,33 @@ train_generator = TrainingGenerator(generator=data_generator,
 train_generator.get_config()
 
 
-n_keypoints = data_generator.keypoints_shape[0]
-batch = train_generator(batch_size=1, validation=False)[0]
-inputs = batch[0]
-outputs = batch[1]
-
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10,10))
-ax1.set_title('image')
-ax1.imshow(inputs[0,...,0], cmap='gray', vmin=0, vmax=255)
-
-ax2.set_title('posture graph')
-ax2.imshow(outputs[0,...,n_keypoints:-1].max(-1))
-
-ax3.set_title('keypoints confidence')
-ax3.imshow(outputs[0,...,:n_keypoints].max(-1))
-
-ax4.set_title('posture graph and keypoints confidence')
-ax4.imshow(outputs[0,...,-1], vmin=0)
-plt.show()
+#n_keypoints = data_generator.keypoints_shape[0]
+#batch = train_generator(batch_size=1, validation=False)[0]
+#inputs = batch[0]
+#outputs = batch[1]
+#
+#fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10,10))
+#ax1.set_title('image')
+#ax1.imshow(inputs[0,...,0], cmap='gray', vmin=0, vmax=255)
+#
+#ax2.set_title('posture graph')
+#ax2.imshow(outputs[0,...,n_keypoints:-1].max(-1))
+#
+#ax3.set_title('keypoints confidence')
+#ax3.imshow(outputs[0,...,:n_keypoints].max(-1))
+#
+#ax4.set_title('posture graph and keypoints confidence')
+#ax4.imshow(outputs[0,...,-1], vmin=0)
+#plt.show()
 
 train_generator.on_epoch_end()
 
 
 
 #model = LEAP(train_generator)
+model = DeepLabCut(train_generator)
 
 
-model = LEAP(train_generator)
 
 
 data_size = (10000,) + data_generator.image_shape
@@ -132,38 +132,44 @@ y = model.predict(x[:100], batch_size=100) # make sure the model is in GPU memor
 t0 = time.time()
 y = model.predict(x, batch_size=100, verbose=1)
 t1 = time.time()
-print(x.shape[0] / (t1 - t0))
+print((t1 - t0) / x.shape[0])
 
+#t0 = time.time()
+#for i in range(100):
+#    X, y = train_generator[i]
+#t1 = time.time()
+#print("Time per batch: ", (t1 - t0) / 100)
 
 
 logger = Logger(validation_batch_size=10,
                     # filepath saves the logger data to a .h5 file
-                    # filepath=HOME +
-                # "/deepposekit-data/datasets/fly/log_densenet.h5"
+                    filepath="/media/storage2/open_monkey/log_deep.h5"
                 )
 
 
-reduce_lr = ReduceLROnPlateau(monitor="val_loss", factor=0.2, verbose=1, patience=20)
+reduce_lr = ReduceLROnPlateau(monitor="val_loss", factor=0.2, verbose=2, patience=20)
 
 
 model_checkpoint = ModelCheckpoint(
-        "/media/storage2/open_monkey/checkpoints/best_LEAP.h5",
+        "/media/storage2/open_monkey/checkpoints/best_DLC.h5",
         monitor="val_loss",
         # monitor="loss" # use if validation_split=0
-        verbose=1,
+        verbose=2,
         save_best_only=True,
 )
+
+model.distribute_strategy = tf.distribute.MirroredStrategy()
 
 early_stop = EarlyStopping(
         monitor="val_loss",
         # monitor="loss" # use if validation_split=0
         min_delta=0.001,
         patience=100,
-        verbose=1
+        verbose=2
 )
-callbacks = [early_stop, reduce_lr, model_checkpoint, logger]
+callbacks = [model_checkpoint, logger]
 
-
+print("starting fit")
 model.fit(
         batch_size=16,
         validation_batch_size=10,
@@ -172,5 +178,4 @@ model.fit(
         epochs=200,
         n_workers=8,
         steps_per_epoch=None,
-        use_multiprocessing=True
 )
